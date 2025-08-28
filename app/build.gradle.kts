1475cb43893dc262d9b5dc32710b5df32138bdc0
Hecta18 <62799494+Hecta18@users.noreply.github.com>
2025-08-28 13:32:46 -0600

    Mejora manejo librerias

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -40,9 +40,7 @@ android {
 }
 
 dependencies {
-
     implementation(libs.androidx.core.ktx)
-
     implementation(libs.androidx.lifecycle.runtime.ktx)
     implementation(libs.androidx.activity.compose)
     implementation(platform(libs.androidx.compose.bom))
@@ -50,6 +48,8 @@ dependencies {
     implementation(libs.androidx.ui.graphics)
     implementation(libs.androidx.ui.tooling.preview)
     implementation(libs.androidx.material3)
+    implementation(libs.kotlinx.collections.immutable)
+    implementation(libs.coil.compose)
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
@@ -57,6 +57,4 @@ dependencies {
     androidTestImplementation(libs.androidx.ui.test.junit4)
     debugImplementation(libs.androidx.ui.tooling)
     debugImplementation(libs.androidx.ui.test.manifest)
-    implementation(libs.coil.compose)
-    implementation("org.jetbrains.kotlinx:kotlinx-collections-immutable:0.4.0")
 }
